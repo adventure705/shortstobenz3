77451c274fb2281795cad312fd99ce87b1421848
adventure705 <moonnyung@gmail.com>
2025-12-26 17:59:03 +0900

    Add Shorts Explosion lecture content and sidebar item

--- start_server.bat
+++ start_server.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting local web server...
-echo Please open your browser to http://localhost:8000/shortstobenz3.html
+echo Please open your browser to http://localhost:8000/index.html
 cd /d "%~dp0"
 python -m http.server 8000
 pause
